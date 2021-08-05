57919d6901a77bddea89907854a5ee1f19037a33
Huc <luca.ucciero@gmail.com>
2021-08-05 17:23:47 +0200

    feat: added vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
@@ -21,5 +22,6 @@ const router = new VueRouter({
 
 new Vue({
   router,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
