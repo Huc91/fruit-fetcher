1d19e17b4d0c3019316e9b56f284a99c40ac09f7
Huc <luca.ucciero@gmail.com>
2021-08-05 16:31:04 +0200

    feat: getting data

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,6 +12,17 @@ export default {
         loading: false,
       };
   },
+  methods: {
+    async getFruits(){
+      const baseUrl = 'http://localhost:3000';
+      const rawData = await fetch(`${baseUrl}/fruit`);
+      const data = await rawData.json();
+      console.log(data);
+    }
+  },
+  created() {
+    this.getFruits();
+  }
 };
 </script>
 
