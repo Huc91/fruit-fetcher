a149fda570a366f2274954d12dfbdbae60fac4cd
Huc <luca.ucciero@gmail.com>
2021-08-06 12:40:30 +0200

    feat: store test unit completed

--- src/store/store.js
+++ src/store/store.js
@@ -3,23 +3,29 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const state = {
+  fruits: null,
+  fruitDetails: null,
+};
+
+export const mutations = {
+  setFruits(state, fruits) {
+    state.fruits = fruits
+  },
+  setDetail(state, fruitDetails) {
+    state.fruitDetails = fruitDetails
+  },
+};
+
+export const getters = {
+  fruits: state => state.fruits,
+  fruitDetails: state => state.fruitDetails
+}
+
 const store = new Vuex.Store({
-  state: {
-    fruits: null,
-    fruitDetails: null,
-  },
-  mutations: {
-    setFruits(state, fruits) {
-      state.fruits = fruits
-    },
-    setDetail(state, fruitDetails) {
-      state.fruitDetails = fruitDetails
-    },
-  },
-  getters: {
-    fruits: state => state.fruits,
-    fruitDetails: state => state.fruitDetails
-  }
+  state,
+  mutations,
+  getters,
 })
 
 export default store;
